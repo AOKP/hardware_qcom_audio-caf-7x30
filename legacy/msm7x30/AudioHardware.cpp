45a7b4b70e9d2d639798c293984e013b8dc9ffba
Rudolf Tammekivi <rtammekivi@gmail.com>
2013-06-06 12:07:51 +0300

    audio/msm7x30: Allow devices with broadside dual mic to function.
    If the device has both dual mics, endfire will be used.
    
    Change-Id: Id6a197e85b159bb4f5452553e3b6a417bd1b9db4

--- legacy/msm7x30/AudioHardware.cpp
+++ legacy/msm7x30/AudioHardware.cpp
@@ -113,8 +113,8 @@ static uint32_t DEVICE_HEADSET_TX = 5; //headset_mono_tx
 static uint32_t DEVICE_FMRADIO_HANDSET_RX= 6; //fmradio_handset_rx
 static uint32_t DEVICE_FMRADIO_HEADSET_RX= 7; //fmradio_headset_rx
 static uint32_t DEVICE_FMRADIO_SPEAKER_RX= 8; //fmradio_speaker_rx
-static uint32_t DEVICE_DUALMIC_HANDSET_TX = 9; //handset_dual_mic_endfire_tx
-static uint32_t DEVICE_DUALMIC_SPEAKER_TX = 10; //speaker_dual_mic_endfire_tx
+static uint32_t DEVICE_DUALMIC_HANDSET_TX = 9; //handset_dual_mic_broadside_tx/handset_dual_mic_endfire_tx
+static uint32_t DEVICE_DUALMIC_SPEAKER_TX = 10; //speaker_dual_mic_broadside_tx/speaker_dual_mic_endfire_tx
 static uint32_t DEVICE_TTY_HEADSET_MONO_RX = 11; //tty_headset_mono_rx
 static uint32_t DEVICE_TTY_HEADSET_MONO_TX = 12; //tty_headset_mono_tx
 static uint32_t DEVICE_BT_SCO_RX = 17; //bt_sco_rx
@@ -535,8 +535,12 @@ AudioHardware::AudioHardware() :
                 index = DEVICE_FMRADIO_HEADSET_RX;
             else if(strcmp((char* )name[i],"fmradio_speaker_rx") == 0)
                 index = DEVICE_FMRADIO_SPEAKER_RX;
+            else if(strcmp((char* )name[i],"handset_dual_mic_broadside_tx") == 0)
+                index = DEVICE_DUALMIC_HANDSET_TX;
             else if(strcmp((char* )name[i],"handset_dual_mic_endfire_tx") == 0)
                 index = DEVICE_DUALMIC_HANDSET_TX;
+            else if(strcmp((char* )name[i],"speaker_dual_mic_broadside_tx") == 0)
+                index = DEVICE_DUALMIC_SPEAKER_TX;
             else if(strcmp((char* )name[i],"speaker_dual_mic_endfire_tx") == 0)
                 index = DEVICE_DUALMIC_SPEAKER_TX;
             else if(strcmp((char* )name[i],"tty_headset_mono_rx") == 0)
